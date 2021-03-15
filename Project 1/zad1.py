6a170294e88551d78f6e77b92519bcf67928e265
Kamil Sudoł <61806373+kamilsudol@users.noreply.github.com>
2021-03-15 20:21:43 +0100

    all conversions added

--- Project 1/zad1.py
+++ Project 1/zad1.py
@@ -5,6 +5,40 @@ from igraph import *
 import numpy as np
 
 
+def incidence_matrix_to_list(matrix):
+    converted_matrix = [[] for i in range(len(matrix))]
+    tmp_matrix = np.array(matrix, dtype=int).transpose()
+    for row in tmp_matrix:
+        first_flag = False
+        first_idx = 0
+        second_idx = 0
+        tmp_idx = 0
+        for x in row:
+            if x == 1 and first_flag is False:
+                first_flag = True
+                first_idx = tmp_idx
+            elif x == 1 and first_flag:
+                second_idx = tmp_idx
+            tmp_idx += 1
+        converted_matrix[first_idx].append(second_idx + 1)
+        converted_matrix[second_idx].append(first_idx + 1)
+    return converted_matrix
+
+
+def list_to_incidence_matrix(matrix):
+    converted_matrix = []
+    tmp_idx = 0
+    for row in matrix:
+        for x in row:
+            new_row = [0 for i in range(len(matrix))]
+            new_row[tmp_idx] = 1
+            new_row[int(x) - 1] = 1
+            converted_matrix.append(new_row)
+        tmp_idx += 1
+    converted_matrix = np.unique(np.array(converted_matrix).transpose(), axis=1)
+    return converted_matrix
+
+
 def list_to_adjacency_matrix(matrix):
     converted_matrix = np.zeros((len(matrix), len(matrix)), dtype=int)
     tmp_idx = 0
@@ -198,6 +232,10 @@ if __name__ == '__main__':
     # plot_igraph_on_circle(g)
     # file_reader("m_incydencji.txt")
     matrix_obj = file_reader("l_sasiedztwa.txt")
-    converted = list_to_adjacency_matrix(matrix_obj[0])
-    adjacency_matrix_to_list(converted)
+    # converted = list_to_adjacency_matrix(matrix_obj[0])
+    # adjacency_matrix_to_list(converted)
+    converted = list_to_incidence_matrix(matrix_obj[0])
+    incidence_matrix_to_list(converted)
+    # g = create_igraph_from_incidence_matrix(converted)
+    # plot_igraph_on_circle(g)
     # file_reader("m_sasiedztwa.txt")
