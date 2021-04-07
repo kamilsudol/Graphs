cf5257cc2b5208183042884f677a9b7f9591fb59
Michał Kuś <kusmichal06@gmail.com>
2021-04-07 14:21:52 +0200

    Random graph generation

--- Project 1/main.py
+++ Project 1/main.py
@@ -3,6 +3,7 @@ from plot_igraph_on_circle import plot_igraph_on_circle
 from matrix_conversions import *
 from igraph_creation import *
 from MatrixRepresentation import MatrixRepresentation
+from random_graph import *
 
 if __name__ == '__main__':
     filename = input("Wskaż nazwę pliku zawierającego macierz w formie\n - listy sąsiedztwa\n - macierzy incydencji\n - macierzy sąsiedztwa\n> ")
@@ -20,3 +21,19 @@ if __name__ == '__main__':
         print('[[' + ']\n ['.join( [' '.join( [str(cell) for cell in row] ) for row in converted] ) + ']]\n')
     else:
         print(np.matrix(converted))
+
+    confirm_random = input("Czy chcesz wygenerowac graf losowy?    tak/nie\n")
+    if confirm_random == "tak":
+        random_graph = []
+        num_vertices = int(input("Podaj liczbe wierzcholkow grafu.\n"))
+        random_method = input("Podaj metode generacji grafu [G(n, l) czy G(n, p)].    l/p\n")
+        if random_method == "l":
+            num_edges = int(input("Podaj liczbe krawedzi grafu.\n"))
+            random_graph_adj = random_graph_edges(num_vertices, num_edges)
+        elif random_method == "p":
+            probability = float(input("Podaj prawdopodbienstwo istnienia krawedzi.\n"))
+            random_graph_adj = random_graph_probability(num_vertices, probability)
+
+        print(np.matrix(random_graph_adj))
+        random_graph = MatrixRepresentation.AdjacencyMatrix.to_igraph_func()(random_graph_adj)
+        plot_igraph_on_circle(random_graph)
---
+++ Project 1/random_graph.py
@@ -0,0 +1,48 @@
+import numpy as np
+import random as rng
+
+def adj_matrix_init_zero(num_vertices):
+    matrix = []
+
+    for i in range(num_vertices):
+        matrix.insert(i, [])
+        for j in range(num_vertices):
+            matrix[i].append(0)
+
+    return matrix
+
+def random_graph_edges(num_vertices, num_edges):
+    matrix = adj_matrix_init_zero(num_vertices)
+    rng.seed()
+
+    for x in range(num_edges):
+        row = rng.randint(1, num_vertices-1)
+        col = rng.randint(0, row-1)
+
+        # print("------------------")
+        # print(np.matrix(matrix))
+        # print("[{}, {}]".format(row, col))
+
+        while matrix[row][col] != 0:
+            row = rng.randint(1, num_vertices-1)
+            col = rng.randint(0, row-1)
+            # print("[{}, {}] = {}".format(row, col, matrix[row][col]))
+
+        matrix[row][col] = 1
+        matrix[col][row] = 1
+
+    return matrix
+
+
+def random_graph_probability(num_vertices, probability):
+    matrix = adj_matrix_init_zero(num_vertices)
+    rng.seed()
+
+    for i in range(num_vertices):
+        for j in range(i-1):
+            if rng.random() <= probability:
+                matrix[i][j] = 1
+                matrix[j][i] = 1
+
+    return matrix
+
