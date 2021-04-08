619e678d6d4d1a00b6d9fc4e0de0b4ef18904f00
Przemyslaw Skrobot <44038381+frogi16@users.noreply.github.com`>
2021-04-08 22:19:33 +0200

    Error-proof project 2

--- Project 2/main.py
+++ Project 2/main.py
@@ -8,6 +8,9 @@ do_nothing = lambda : None
 exercises = [test_graphic_sequence, test_randomization, find_largest_connected_component, do_nothing, generate_and_show_random_k_connected_graph]
 
 while True:
-    exercise_index = int(input("Wybierz funkcje:\n - 1 - rysuj ciag graficzny\n - 2 - przeprowadz randomizacje grafu \n - 3 - znajdz najwieksze spojne skladowe na grafie\n - \n - 5 - generuj losowy graf k-spojny\n> ").strip()) - 1
-    exercises[exercise_index]()
-    print()
+    try:
+        exercise_index = int(input("Wybierz funkcje:\n - 1 - rysuj ciag graficzny\n - 2 - przeprowadz randomizacje grafu \n - 3 - znajdz najwieksze spojne skladowe na grafie\n - \n - 5 - generuj losowy graf k-spojny\n> ").strip()) - 1
+        exercises[exercise_index]()
+        print()
+    except:
+        print("Wystąpił błąd\n")
