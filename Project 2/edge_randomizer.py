fa03bc88d523cd61cc844091deb3de633847dcc3
Michał Kuś <kusmichal06@gmail.com>
2021-04-08 14:07:28 +0200

    Edge randomization

---
+++ Project 2/edge_randomizer.py
@@ -0,0 +1,85 @@
+import graphic_sequence as gseq
+import random as rng
+import numpy as np
+
+import sys
+import os
+sys.path.append(os.path.abspath('Project 1'))   # łeee
+
+from matrix_conversions import adjacency_matrix_to_incidence_matrix
+from igraph_creation import create_igraph_from_incidence_matrix
+from plot_igraph_on_circle import plot_igraph_on_circle
+
+def roll_edges(inc, num_vertices):
+    rng.seed()
+
+    edge_one_col = -1
+    edge_two_col = -1
+    edge_one_vertices = [-1, -1]
+    edge_two_vertices = [-1, -1]
+
+    while edge_one_col == edge_two_col or edge_one_vertices[0] == edge_two_vertices[1] or edge_one_vertices[1] == edge_two_vertices[0]:
+        # print("roll edges\n")
+        edge_one_col = rng.randint(0, num_vertices)
+        edge_two_col = rng.randint(0, num_vertices)
+
+        edge_one_vertices = find_vertices(num_vertices, inc, edge_one_col)
+        edge_two_vertices = find_vertices(num_vertices, inc, edge_two_col)
+        # print("col1:" + str(edge_one_col) + str(edge_one_vertices))
+        # print("col2:" + str(edge_two_col) + str(edge_two_vertices))
+
+    return [edge_one_col, edge_two_col, edge_one_vertices, edge_two_vertices]
+
+def swap_between_columns(row, col1, col2, matrix):
+    temp = matrix[row][col1]
+    matrix[row][col1] = matrix[row][col2]
+    matrix[row][col2] = temp
+
+def find_vertices(num_vertices, inc_matrix, edge_index):
+    vertices = [-1, -1]
+    for row in range(num_vertices):
+        if inc_matrix[row][edge_index] == 1:
+            if vertices[0] == -1:
+                vertices[0] = row
+            else:
+                vertices[1] = row
+    return vertices
+
+def randomize_edges(graphic_seq, num_shuffles):
+    adj = gseq.is_graphic_sequence(graphic_seq)
+
+    if adj is not False:
+        print("Ciag jest graficzny")
+        num_vertices = len(graphic_seq)
+        
+        graph = gseq.create_igraph_from_adjacency_matrix(adj)
+        gseq.plot_igraph_on_circle(graph)
+
+        inc = adjacency_matrix_to_incidence_matrix(adj)
+        
+        for i in range(num_shuffles):
+            edge_one_col, edge_two_col, edge_one_vertices, edge_two_vertices = roll_edges(inc, num_vertices)
+
+            if edge_one_vertices[0] == edge_two_vertices[0]:
+                swap_between_columns(edge_one_vertices[1], edge_one_col, edge_two_col, inc)
+                swap_between_columns(edge_two_vertices[1], edge_one_col, edge_two_col, inc)
+            else:
+                swap_between_columns(edge_one_vertices[0], edge_one_col, edge_two_col, inc)
+                swap_between_columns(edge_two_vertices[0], edge_one_col, edge_two_col, inc)
+
+        return inc
+    else:
+        print("Ciag nie jest graficzny")
+
+def test_randomization():
+    num_shuffles = int(input("Podaj liczbe randomizacji.\n"))
+    graph_seq = gseq.load_sequence()
+    adj = gseq.is_graphic_sequence(graph_seq)
+
+    start_incidence = adjacency_matrix_to_incidence_matrix(adj)
+    print(np.matrix(start_incidence))
+
+    randomized_incidence = randomize_edges(graph_seq, num_shuffles)
+    print(np.matrix(randomized_incidence))
+    graph = create_igraph_from_incidence_matrix(randomized_incidence)
+    plot_igraph_on_circle(graph)
--- Project 2/main.py
+++ Project 2/main.py
@@ -1,10 +1,12 @@
 from graphic_sequence import test_graphic_sequence
+from edge_randomizer import test_randomization
 from random_k_connected_graph import generate_and_show_random_k_connected_graph
 
+
 do_nothing = lambda : None
-exercises = [test_graphic_sequence, do_nothing, do_nothing, do_nothing, generate_and_show_random_k_connected_graph]
+exercises = [test_graphic_sequence, test_randomization, do_nothing, do_nothing, generate_and_show_random_k_connected_graph]
 
 while True:
-    exercise_index = int(input("Wybierz funkcje:\n - 1 - rysuj ciag graficzny\n - \n - \n - \n - 5 - generuj losowy graf k-spojny\n> ").strip()) - 1
+    exercise_index = int(input("Wybierz funkcje:\n - 1 - rysuj ciag graficzny\n - 2 - przeprowadz randomizacje grafu \n - \n - \n - 5 - generuj losowy graf k-spojny\n> ").strip()) - 1
     exercises[exercise_index]()
     print()
