09cbc4fe5dc51fffde0e7f2116763537865559fb
Przemyslaw Skrobot <44038381+frogi16@users.noreply.github.com`>
2021-04-08 22:18:45 +0200

    Improve graph vertices plotting
     - change text color to white to increase contrast
     - scale vertices size based on distance between them
     - tune scaling_factor of plot

--- Project 1/plot_igraph_on_circle.py
+++ Project 1/plot_igraph_on_circle.py
@@ -4,7 +4,7 @@ import math
 
 # output fits inside [0, 1] x [0, 1] square
 def span_vertices_on_circle(num_of_vertices):
-    scaling_factor = 0.5  # bigger => more space outside main, invisible circle and boundaries of region
+    scaling_factor = 0.2  # bigger => more space outside main, invisible circle and boundaries of region
 
     vertices = []
     for i in range(num_of_vertices):
@@ -17,15 +17,19 @@ def span_vertices_on_circle(num_of_vertices):
     return vertices
 
 
+def distance(p1, p2):
+    return math.sqrt(math.pow(p1[0] - p2[0], 2) + math.pow(p1[1] - p2[1], 2))
+
+
 def plot_vertices(vertices, ax):
-    circle_radius = 0.04  # 4% of plot width.
+    circle_radius = min(distance(vertices[0], vertices[1]) / 2.3, 0.04)
     label_from_1 = 1
 
     for i in range(len(vertices)):
         vertex = plt.Circle((vertices[i][0], vertices[i][1]), circle_radius, color='r', zorder=10)
         ax.add_patch(vertex)
         plt.text(vertices[i][0], vertices[i][1], str(i + label_from_1), ha='center', va='center', fontsize='medium',
-                 zorder=11)
+                 color='w', zorder=11)
 
 
 def plot_edges(vertices, es):
