805cde31574cf79396f9f3ea1e0e487d4e8456e0
Przemyslaw Skrobot <44038381+frogi16@users.noreply.github.com`>
2021-04-08 23:52:08 +0200

    Fix possible infinite loop in 2.5
     - randomizing edges can't be completely error-proofed
    with simple ifs - introduce maximum number of reretries
     - print number of retries for testing purposes
    (may be interesting for user)

--- Project 2/random_k_connected_graph.py
+++ Project 2/random_k_connected_graph.py
@@ -9,7 +9,21 @@ sys.path.append(os.path.abspath('Project 1'))   #hacky, maybe add proper modules
 from igraph_creation import create_igraph_from_adjacency_matrix
 from plot_igraph_on_circle import plot_igraph_on_circle
 
+def generate_index_not(index_to_avoid, adj_matrix, seq):
+    index = rng.randint(0, len(seq) - 1)
+    for reretries in range(100):
+        if index == index_to_avoid or (adj_matrix[ seq[index]['vertex'] ][ seq[index_to_avoid]['vertex'] ] == 1):
+            index = rng.randint(0, len(seq) - 1)
+        else:
+            return index
+
+    return None
+
+
 def generate_random_k_connected_graph(vertices, k):
+    if vertices*k % 2 == 1:
+        return None
+
     seq = recfunctions.merge_arrays([[k]*vertices, range(vertices)])
     seq.dtype.names = ['neighbors', 'vertex']
     adj_matrix = np.zeros((len(seq), len(seq)))
@@ -17,15 +31,14 @@ def generate_random_k_connected_graph(vertices, k):
     for retries in range(100):
         while True:
             if len(seq) == 0:
+                print("Ilość prób: " + str(retries + 1))
                 return adj_matrix
-            elif len(seq) == 1:
-                break
 
             index_from = rng.randint(0, len(seq) - 1)
-        
-            index_to = rng.randint(0, len(seq) - 1)
-            while index_from == index_to or (adj_matrix[ seq[index_from]['vertex'] ][ seq[index_to]['vertex'] ] == 1):
-                index_to = rng.randint(0, len(seq) - 1)
+            index_to = generate_index_not(index_from, adj_matrix, seq)
+
+            if index_to is None:
+                break
 
             adj_matrix[ seq[index_from]['vertex'] ][ seq[index_to]['vertex'] ] = adj_matrix[ seq[index_to]['vertex'] ][ seq[index_from]['vertex'] ] = 1
         
