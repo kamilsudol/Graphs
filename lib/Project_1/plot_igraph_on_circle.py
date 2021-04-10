adc544789e7c35590779613b457487f3c10bef25
Przemyslaw Skrobot <44038381+frogi16@users.noreply.github.com`>
2021-04-10 17:59:38 +0200

    Improve drawing of vertex number text
     - add black outline to maintain visibility on different backgrounds
       (required from d24bb88b)
     - compute font size based on circle_radius and ax dimensions
     - introduce TextResizer to scale text when window is resized

--- lib/Project_1/plot_igraph_on_circle.py
+++ lib/Project_1/plot_igraph_on_circle.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
+import matplotlib.patheffects as path_effects
 import math
 
+from .TextResizer import *
 
 # output fits inside [0, 1] x [0, 1] square
 def span_vertices_on_circle(num_of_vertices):
@@ -25,13 +27,19 @@ def plot_vertices(vertices, ax, colormap):
     circle_radius = min(distance(vertices[0], vertices[1]) / 2.3, 0.04)
     label_from_1 = 1
     if colormap is None:
-        colormap = ['r' for i in range(len(vertices) + 1)]
+        colormap = ['r']  * (len(vertices) + 1)
 
+    texts = []
     for i in range(len(vertices)):
         vertex = plt.Circle((vertices[i][0], vertices[i][1]), circle_radius, color=colormap[i + label_from_1], zorder=10)
         ax.add_patch(vertex)
-        plt.text(vertices[i][0], vertices[i][1], str(i + label_from_1), ha='center', va='center', fontsize='medium',
-                 color='w', zorder=11)
+
+        size = circle_radius * min(ax.bbox.height, ax.bbox.width)
+        texts.append(ax.text(vertices[i][0], vertices[i][1], str(i + label_from_1), ha='center', va='center', fontsize=size,
+                     color='w', zorder=11))
+        texts[-1].set_path_effects([path_effects.Stroke(linewidth=1, foreground='black'), path_effects.Normal()])
+
+    cid = plt.gcf().canvas.mpl_connect("resize_event", TextResizer(texts))
 
 
 def plot_edges(vertices, es):
