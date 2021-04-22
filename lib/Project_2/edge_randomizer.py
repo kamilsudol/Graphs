83849ebec9b29df17be9cdeed524fd381d97afca
Michał Kuś <kusmichal06@gmail.com>
2021-04-22 22:39:41 +0200

    2.2 and 2.4 documentation

--- lib/Project_2/edge_randomizer.py
+++ lib/Project_2/edge_randomizer.py
@@ -7,7 +7,10 @@ from lib.Project_1.matrix_conversions import adjacency_matrix_to_incidence_matri
 from lib.Project_1.igraph_creation import create_igraph_from_incidence_matrix
 from lib.Project_1.plot_igraph_on_circle import plot_igraph_on_circle
 
-
+# Pick two random edges from incidence matrix
+# inc -> graph as incidence matrix
+# num_vertices -> number of vertices in a graph (number of rows)
+# Returns array of data in order edge one column, edge two column, edge one vertices, edge two vertices
 def roll_edges(inc, num_vertices):
     rng.seed()
 
@@ -16,55 +19,74 @@ def roll_edges(inc, num_vertices):
     edge_one_vertices = [-1, -1]
     edge_two_vertices = [-1, -1]
 
+    # Pick two different edges
     while edge_one_col == edge_two_col or edge_one_vertices[0] == edge_two_vertices[1] or edge_one_vertices[1] == edge_two_vertices[0]:
-        # print("roll edges\n")
         edge_one_col = rng.randint(0, num_vertices)
         edge_two_col = rng.randint(0, num_vertices)
 
         edge_one_vertices = find_vertices(num_vertices, inc, edge_one_col)
         edge_two_vertices = find_vertices(num_vertices, inc, edge_two_col)
-        # print("col1:" + str(edge_one_col) + str(edge_one_vertices))
-        # print("col2:" + str(edge_two_col) + str(edge_two_vertices))
 
     return [edge_one_col, edge_two_col, edge_one_vertices, edge_two_vertices]
 
+# Swaps vertex to another edge
+# row -> row index of vertex to be swapped
+# col1 -> column index of edge one 
+# col2 -> column index of edge two
+# matrix -> graph as incidence matrix
 def swap_between_columns(row, col1, col2, matrix):
     temp = matrix[row][col1]
     matrix[row][col1] = matrix[row][col2]
     matrix[row][col2] = temp
 
-
+# Finds vertices in an edge
+# num_vertices -> number of vertices (number of rows)
+# inc_matrix -> graph as incidence matrix
+# edge_index -> column index of an edge
+# Returns indices of vertices in an edge as an array [v1, v2] 
 def find_vertices(num_vertices, inc_matrix, edge_index):
     vertices = [-1, -1]
     for row in range(num_vertices):
+        # If cell in column is a vertex
         if inc_matrix[row][edge_index] == 1:
+            # and haven't found vertex one
             if vertices[0] == -1:
+                # Save vertex one
                 vertices[0] = row
             else:
+                # Save vertex two
                 vertices[1] = row
     return vertices
 
-
+# Find adjacency matrix from a graphic sequence and randomize it
+# by swapping vertices between edges
+# graphic_seq -> graph as graphic sequence
+# num_shuffles -> number of vertex shuffles to be executed
+# Returns an array as [is_graphic, inc]
+# where is_graphic is either True/False
+# and inc is the modified incidence matrix 
 def randomize_edges(graphic_seq, num_shuffles):
-    adj = gseq.is_graphic_sequence(graphic_seq)
     is_graphic = False
+    # Check if graphic sequence is in fact graphic,
+    # and if so return the adjacency matrix of that graphic sequence
+    adj = gseq.is_graphic_sequence(graphic_seq)
 
     if adj is not False:
         is_graphic = True
         num_vertices = len(graphic_seq)
-        
-        # graph = gseq.create_igraph_from_adjacency_matrix(adj)
-        # gseq.plot_igraph_on_circle(graph)
 
         inc = adjacency_matrix_to_incidence_matrix(adj)
         
-        for i in range(num_shuffles):
+        for _ in range(num_shuffles):
             edge_one_col, edge_two_col, edge_one_vertices, edge_two_vertices = roll_edges(inc, num_vertices)
 
+            # If vertex one in edges one and two is the same vertex
             if edge_one_vertices[0] == edge_two_vertices[0]:
+                # Swap second vertex
                 swap_between_columns(edge_one_vertices[1], edge_one_col, edge_two_col, inc)
                 swap_between_columns(edge_two_vertices[1], edge_one_col, edge_two_col, inc)
             else:
+                # Swap first vertex
                 swap_between_columns(edge_one_vertices[0], edge_one_col, edge_two_col, inc)
                 swap_between_columns(edge_two_vertices[0], edge_one_col, edge_two_col, inc)
 
--- lib/Project_2/eulerian_cycle.py
+++ lib/Project_2/eulerian_cycle.py
@@ -6,23 +6,36 @@ from . import edge_randomizer as edrng
 from . import largest_connected_component as largcomp
 import random as rng
 
-
+# Check if the incidence matrix already contains an edge which you want to add
+# inc -> incidence matrix
+# v_one -> row index of vertex one in incidence matrix 
+# v_two -> row index of vertex two in incidence matrix 
+# checked_edge -> column index of the checked edge
+# Returns True if there is a duplicate, False otherwise
 def is_duplicate_edge(inc, v_one, v_two, checked_edge):
+    # Get row indexes of vertices in checked edge
     [vertex_one_checked, _] = edrng.find_vertices(len(inc), inc, checked_edge)
     vertex_two_checked = v_two
     for edge in range(len(inc[v_one])):
         if edge != checked_edge:
+            #  Get row indexes of vertices in current edge
             [vertex_one_temp, vertex_two_temp] = edrng.find_vertices(len(inc), inc, edge)
 
+            # Check if the vertices in current edge are the same as those in checked edge
             if vertex_one_checked == vertex_one_temp and vertex_two_checked == vertex_two_temp:
                 return True
     return False
 
+# Get graphic sequence from incidence matrix
+# inc -> incidence matrix
+# num_vertices -> number of vertices in the incidence matrix(number of rows)
+# Returns graphic sequence as list
 def calculate_graphic_seq_from_incidence(inc, num_vertices):
     graphic_seq = []
     vertex_degree = 0
 
     for row in range(num_vertices):
+        # Calculate the degree of a single vertex by adding all edges that it belongs to
         for vertex in inc[row]:
             vertex_degree += vertex
         graphic_seq.append(vertex_degree)
@@ -30,29 +43,46 @@ def calculate_graphic_seq_from_incidence(inc, num_vertices):
     
     return graphic_seq
 
+# Rewire edge by changing to which vertices it connects
+# inc -> incidence matrix
+# v_one -> row index of vertex one in incidence matrix
+# v_two -> row index of vertex two in incidence matrix
+# Returns modified incidence matrix
 def rewire_edge(inc, v_one, v_two):
     v_one_deg = 0
     v_two_deg = 0
 
+    # Get degree of vertex one and two by adding the edges they belong to
     for edge in range(len(inc[v_one])):
         v_one_deg += inc[v_one][edge]
         v_two_deg += inc[v_two][edge]
 
+    # If degree of vertex one is bigger than degree of vertex two and for each edge in incidence matrix
     if v_one_deg > v_two_deg:
         for edge in range(len(inc[v_one])):
+            # If the edge connects to vertex one and doesn't connect to vertex two and edge between one and two doesn't exist
             if inc[v_one][edge] == 1 and inc[v_two][edge] == 0 and not is_duplicate_edge(inc, v_one, v_two, edge):
+                # Disconnect edge from vertex one
                 inc[v_one][edge] = 0
+                # Connect the edge to vertex two
                 inc[v_two][edge] = 1
                 break
+    # If degree of vertex two is bigger or equal than degree of vertex one and for each edge in incidence matrix
     else:
         for edge in range(len(inc[v_two])):
+            # If the edge connects to vertex two and doesn't connect to vertex one and edge between one and two doesn't exist
             if inc[v_two][edge] == 1 and inc[v_one][edge] == 0 and not is_duplicate_edge(inc, v_two, v_one, edge):
+                # Disonnect the edge from vertex two
                 inc[v_two][edge] = 0
+                # Connect the edge to vertex one
                 inc[v_one][edge] = 1
                 break
 
     return inc
 
+# Checks if graphic sequence meets the condition of containing only vertices with even degrees
+# graphic_seq -> graphic sequence to be evaluated
+# Returns True if condition met and False otherwise
 def is_eulerian(graphic_seq):
     eulerian = True
     for vertex_degree in graphic_seq:
@@ -61,6 +91,11 @@ def is_eulerian(graphic_seq):
 
     return eulerian
 
+# Relabels vertices of a graph component to make it a standalone graph
+# Gets rid of missing vertices
+# component_adj_list -> adjacency list of a graph component
+# component_vertex_indexes -> list of vertex labels of a graph component
+# Returns relabeled adjacency list
 def component_to_graph(component_adj_list, component_vertex_indexes):
     num_vertices = len(component_vertex_indexes)
     new_indexes = []
@@ -75,6 +110,9 @@ def component_to_graph(component_adj_list, component_vertex_indexes):
     
     return new_adj_list
 
+# Get the iterator of the largest graph component from components list
+# components_list -> list of graph components which are represented by lists of member vertices
+# Returns the iterator 
 def get_largest_component_iterator(components_list):
     max_len = 0
     max_it = 0
@@ -88,19 +126,33 @@ def get_largest_component_iterator(components_list):
 
     return max_it
 
+# Fix the "all vertex degrees even" condition of graphic sequence
 def fix_graphic_seq_condition(inc, graphic_seq):
     new_graphic_seq = graphic_seq
 
+    # For each vertex one index
     for v_one_idx in range(len(graphic_seq)):
+        # If degree of vertex one is odd
         if graphic_seq[v_one_idx] % 2 != 0:
+            # For each vertex two index
             for v_two_idx in range(v_one_idx+1, len(graphic_seq)):
+                # If degree of vertex two is odd
                 if graphic_seq[v_two_idx] % 2 != 0:
+                    # Rewire an edge between rows of vertex one and two which are both odd
                     inc = rewire_edge(inc, v_one_idx, v_two_idx)
                     new_graphic_seq = calculate_graphic_seq_from_incidence(inc, len(graphic_seq))
                     break
     
     return new_graphic_seq
 
+# Generate a random graph with even number of edges
+# Graph has vertices in range of [min_vert, max_vert]
+# Graph has edges in range of [min_edges, max_edges]
+# min_vert -> minimum number of vertices
+# max_vert -> maximum number of vertices
+# min_edges -> minimum number of edges
+# max_edges -> maximum number of edges
+# Returns the graph as adjacency list
 def get_random_eulerian_candidate(min_vert, max_vert, min_edges, max_edges):
     rng.seed()
 
@@ -111,11 +163,16 @@ def get_random_eulerian_candidate(min_vert, max_vert, min_edges, max_edges):
 
     return matconv.adjacency_matrix_to_list(rngraph.random_graph_edges(num_vertices, num_edges))
 
+# Finds and relabels the largest component of a graph
+# adj_list -> graph as adjacency list
+# Returns largest component as adjacency list
 def get_candidates_largest_component(adj_list):
+    # Parse graph into connected components and find the largest one 
     components_list = largcomp.components(adj_list)
     largest_iterator = get_largest_component_iterator(components_list)
     largest_component = components_list[largest_iterator]
 
+    # Get largest components adjacency list and list of vertex labels
     largest_adj_list = []
     largest_component_vertex_indexes = []
     for vertex in largest_component:
@@ -124,20 +181,34 @@ def get_candidates_largest_component(adj_list):
 
     return component_to_graph(largest_adj_list, largest_component_vertex_indexes)
 
-def get_random_eulerian_graph():
-    candidate_list = get_random_eulerian_candidate(6, 15, 4, 24)
+# Generates a graph and makes it eulerian
+# Graph has vertices in range of [min_vert, max_vert]
+# Graph has edges in range of [min_edges, max_edges]
+# min_vert -> minimum number of vertices
+# max_vert -> maximum number of vertices
+# min_edges -> minimum number of edges
+# max_edges -> maximum number of edges
+# Returns the graph as incidence matrix 
+# or empty array if it generated a graph that couldn't be made eulerian
+def get_random_eulerian_graph(min_vert, max_vert, min_edges, max_edges):
+    # Fetch adjacency list of a random graph with even number of edges
+    candidate_list = get_random_eulerian_candidate(min_vert, max_vert, min_edges, max_edges)
 
+    # Fetch adjacency list of the largest component of generated graph
     largest_candidate_list = get_candidates_largest_component(candidate_list)
     
+    # Get graphic sequence of generated graph
     graphic_seq = []
     for vertex in largest_candidate_list:
         graphic_seq.append(len(vertex))
     
+    # Get incidence matrix of generated graph
     inc = matconv.list_to_incidence_matrix(largest_candidate_list)
 
     old_graphic_seq = []
     not_eulerian = True
     while not_eulerian:
+        # Fix "all vertex degrees even" condition, if it can't be done return []
         old_graphic_seq = graphic_seq
         graphic_seq = fix_graphic_seq_condition(inc, graphic_seq)
         if old_graphic_seq == graphic_seq:
@@ -147,23 +218,41 @@ def get_random_eulerian_graph():
 
     return inc
 
+# Removes an edge from adjacency list of a graph
+# The edge is removed between v_one and another vertex specified by it's index in v_one's neighbors
+# adj_list -> adjacency list of a graph
+# v_one -> vertex number in adjacency list 
+# other_vertex_idx -> index of the vertex, in v_one row, the edge to which is going to be removed
 def remove_edge(adj_list, v_one, other_vertex_idx):
+    # Fetch vertex two number in adjacency list
     v_two = adj_list[v_one-1][other_vertex_idx]
+    # Remove vertex two from neighbors list of vertex one
     adj_list[v_one-1].remove(v_two)
+    # Remove vertex one from neighbors list of vertex two
     adj_list[v_two-1].remove(v_one)
 
+# Finds the eulerian cycle in a eulerian graph
+# using Hierholzer's algorithm
+# adj_list -> eulerian graph as adjacency list
+# Returns an array containing the cycle  
 def find_eulerian_cycle(adj_list):
     temp_list = adj_list
 
+    # Operate as stacks
     current_tour = []
     eulerian_cycle = []
 
+    # Start current tour at vertex number 1
     current_tour.append(1)
 
     while len(current_tour) > 0:
+        # Set current vertex as the last in current tour
         current_vertex = current_tour.pop()
         current_tour.append(current_vertex)
 
+        # If current vertex has no neighbors 
+        # remove it from current tour and add it to cycle,
+        # otherwise append its first neighbor to current tour and remove the edge between them
         if len(temp_list[current_vertex-1]) == 0:
             eulerian_cycle.append(current_tour.pop())
         else:
@@ -175,7 +264,7 @@ def find_eulerian_cycle(adj_list):
 def test_eulerian_cycle():
     inc = []
     while len(inc) < 2:
-        inc = get_random_eulerian_graph()
+        inc = get_random_eulerian_graph(6, 15, 4, 24)
         if len(inc) != 0:
             print("Ciag graficzny grafu eulerowskiego: ", calculate_graphic_seq_from_incidence(inc, len(inc)))
 
