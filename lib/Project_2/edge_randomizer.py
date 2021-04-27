9d61d832e4051e77317a75cff2f5132034369bc9
Kamil Sudoł <61806373+kamilsudol@users.noreply.github.com>
2021-04-27 16:37:22 +0200

    2.2 documentation update

--- lib/Project_2/edge_randomizer.py
+++ lib/Project_2/edge_randomizer.py
@@ -1,7 +1,4 @@
-import lib.Project_2.graphic_sequence as gseq
-
 import random as rng
-import numpy as np
 
 from lib.Project_1.matrix_conversions import adjacency_matrix_to_incidence_matrix
 from lib.Project_1.igraph_creation import create_igraph_from_incidence_matrix
@@ -12,6 +9,7 @@ from .retrieve_adj_matrix_from_user import retrieve_adjacency_matrix_from_user
 # Pick two random edges from incidence matrix
 # inc -> graph as incidence matrix
 # num_vertices -> number of vertices in a graph (number of rows)
+# num_edges -> number of edges in a graph
 # Returns array of data in order edge one column, edge two column, edge one vertices, edge two vertices
 def roll_edges(inc, num_vertices, num_edges):
     rng.seed()
@@ -62,41 +60,10 @@ def find_vertices(num_vertices, inc_matrix, edge_index):
 
 # Find adjacency matrix from a graphic sequence and randomize it
 # by swapping vertices between edges
-# graphic_seq -> graph as graphic sequence
+# adj-> graph as adjacency matrix
 # num_shuffles -> number of vertex shuffles to be executed
-# Returns an array as [is_graphic, inc]
-# where is_graphic is either True/False
-# and inc is the modified incidence matrix 
-# def randomize_edges(graphic_seq, num_shuffles):
-#     is_graphic = False
-#     # Check if graphic sequence is in fact graphic,
-#     # and if so return the adjacency matrix of that graphic sequence
-#     adj = gseq.is_graphic_sequence(graphic_seq)
-#
-#     if adj is not False:
-#         is_graphic = True
-#         num_vertices = len(graphic_seq)
-#
-#         inc = adjacency_matrix_to_incidence_matrix(adj)
-#
-#         for _ in range(num_shuffles):
-#             edge_one_col, edge_two_col, edge_one_vertices, edge_two_vertices = roll_edges(inc, num_vertices)
-#
-#             # If vertex one in edges one and two is the same vertex
-#             if edge_one_vertices[0] == edge_two_vertices[0]:
-#                 # Swap second vertex
-#                 swap_between_columns(edge_one_vertices[1], edge_one_col, edge_two_col, inc)
-#                 swap_between_columns(edge_two_vertices[1], edge_one_col, edge_two_col, inc)
-#             else:
-#                 # Swap first vertex
-#                 swap_between_columns(edge_one_vertices[0], edge_one_col, edge_two_col, inc)
-#                 swap_between_columns(edge_two_vertices[0], edge_one_col, edge_two_col, inc)
-#
-#         return [is_graphic, inc]
-#     else:
-#         return [is_graphic, []]
-
-
+# Returns an array of graph as incidence matrix
+# where inc is the modified incidence matrix
 def randomize_edges(adj, num_shuffles):
 
     inc = adjacency_matrix_to_incidence_matrix(adj)
