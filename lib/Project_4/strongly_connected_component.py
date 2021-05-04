4139c3dae9bc1bf6282fec1eadfb5e6e0ecb99ea
Michał Kuś <kusmichal06@gmail.com>
2021-05-04 15:50:58 +0200

    4.2 Silnie spójne składowe - algorytm Kosaraju

---
+++ lib/Project_4/strongly_connected_component.py
@@ -0,0 +1,95 @@
+from lib.Project_1.read_data import load_matrix
+from lib.Project_4.digraph_creation import create_digraph_from_adjacency_matrix
+from lib.Project_4.plot_digraph_on_circle import plot_digraph_on_circle
+import lib.Project_4.random_digraph as rngdi
+import lib.Project_2.largest_connected_component as lcc
+
+import numpy as np
+
+time = 0
+visited = []
+processed = []
+component_association = []
+
+def components_recursive(nr, v, graph):
+    global component_association
+    for u in range(len(graph)):
+        if graph[v][u] == 1 and component_association[u] == -1:
+            component_association[u] = nr
+            components_recursive(nr, u, graph)
+
+def dfs_visit(adj, vertex):
+    global time, visited, processed, component_association
+
+    time += 1
+    visited[vertex] = time
+
+    for neighbor in range(len(adj)):
+        if adj[vertex][neighbor] == 1 and visited[neighbor] == -1:
+                dfs_visit(adj, neighbor)
+
+    time += 1
+    processed[vertex] = time
+
+def kosaraju(adj):
+    global time, visited, processed, component_association
+
+    num_vertices = len(adj)
+    visited = [-1] * num_vertices
+    processed = [-1] * num_vertices
+
+    time = 0
+
+    for vertex in range(num_vertices):
+        if visited[vertex] == -1:
+            dfs_visit(adj, vertex)
+
+    transposed = np.transpose(np.array(adj))
+
+    component_association = [-1] * num_vertices
+    component_idx = 0
+
+    for _ in range(num_vertices):
+        vertex_idx = processed.index(max(processed))
+        if component_association[vertex_idx] == -1:
+            component_idx += 1
+            component_association[vertex_idx] = component_idx
+            components_recursive(component_idx, vertex_idx, transposed)
+
+        processed[vertex_idx] = -1
+
+    return component_association
+
+def get_components_vertices(components):
+    components_vertex_list = []
+    for label in range(1,max(components)+1):
+        components_vertex_list.append([])
+        for vertex in range(len(components)):
+            if label == components[vertex]:
+                components_vertex_list[label-1].append(vertex+1)
+    
+    return components_vertex_list
+
+def test_strongly_connected(filename=None, num_vertices=None, probability=None):
+    if num_vertices == None: num_vertices = int(input("Podaj liczbe wierzchołkow grafu: "))
+    if probability == None: probability = float(input("Podaj prawdopodobienstwo istnienia krawedzi: "))
+
+    # get random digraph
+    if filename == None:
+        print("Generuje graf...")
+        adj = rngdi.random_graph_probability(num_vertices, probability)
+    else:
+        adj = load_matrix(filename)
+
+    #find components with Kosaraju
+    print("Szukam składowych...")
+    components = kosaraju(adj)
+    # print(components)
+
+    print("Wynik:")
+    components_vertex_list = get_components_vertices(components)
+    for l in components_vertex_list:
+        print(l)
+
+    colormap = lcc.create_colormap(components_vertex_list)
+    plot_digraph_on_circle(create_digraph_from_adjacency_matrix(adj), colormap)
--- main4.py
+++ main4.py
@@ -1,13 +1,14 @@
 from lib.Project_4.random_digraph import digraph_randomizer_start
+from lib.Project_4.strongly_connected_component import test_strongly_connected
 
 do_nothing = lambda: None
 
 if __name__ == '__main__':
-    exercises = [digraph_randomizer_start, do_nothing]
+    exercises = [digraph_randomizer_start, test_strongly_connected, do_nothing]
 
     while True:
         exercise_index = int(input(
-            "Wybierz funkcje:\n - 1 - losuj graf skierowany\n - 2 - zakoncz\n> ").strip()) - 1
+            "Wybierz funkcje:\n - 1 - losuj graf skierowany\n - 2 - znajdz najwieksza silnie spojna skladowa\n - 3 - zakoncz\n> ").strip()) - 1
         exercises[exercise_index]()
             
         if exercise_index == len(exercises) - 1:
